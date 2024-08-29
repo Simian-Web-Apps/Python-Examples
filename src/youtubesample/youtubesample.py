355956288d1c5dbe67dadce931dcc6fae71ee244
Erik Grassens <erik.grassens@monkeyproofsolutions.nl>
2024-08-29 18:11:52 +0200

    Get api-key from meta data.

--- src/youtubesample/youtubesample.py
+++ src/youtubesample/youtubesample.py
@@ -25,7 +25,7 @@ def gui_init(meta_data: dict) -> dict:
     # Create the form and load the json builder into it.
     Form.componentInitializer(app_pic_hello_world=init_app_toplevel_pic)
     Form.componentInitializer(selection_country=init_selection_country)
-    Form.componentInitializer(selection_category=init_selection_category)
+    Form.componentInitializer(selection_category=get_init_selection_category(meta_data))
     Form.componentInitializer(selection_tag_count=init_selection_tag_count)
     Form.componentInitializer(selection_translation=init_selection_translation)
     Form.componentInitializer(video_list=init_video_list)
@@ -59,16 +59,19 @@ def init_selection_country(comp: component.Select):
     comp.setValues(labels, values, "NL")
 
 
-def init_selection_category(comp: component.Select):
-    # Add trigger-happy event handler to this component.
-    # Populate component with list of categories, fetched from YouTube API.
-    comp.properties = {"debounceTime": 1000, "triggerHappy": "process"}
-    url = "https://www.googleapis.com/youtube/v3/videoCategories?part=snippet&regionCode=NL&key=$APIKEY"
-    response = requests.get(url)
-    datajson = response.json()
-    labels = [item["snippet"]["title"] for item in datajson["items"]]
-    values = [item["id"] for item in datajson["items"]]
-    comp.setValues(labels, values, "28")
+def get_init_selection_category(meta_data: dict):
+    def init_selection_category(comp: component.Select):
+        # Add trigger-happy event handler to this component.
+        # Populate component with list of categories, fetched from YouTube API.
+        comp.properties = {"debounceTime": 1000, "triggerHappy": "process"}
+        url = f"https://www.googleapis.com/youtube/v3/videoCategories?part=snippet&regionCode=NL&key={meta_data["application_data"]["youtube_developer_key"]}"
+        response = requests.get(url)
+        datajson = response.json()
+        labels = [item["snippet"]["title"] for item in datajson["items"]]
+        values = [item["id"] for item in datajson["items"]]
+        comp.setValues(labels, values, "28")
+
+    return init_selection_category
 
 
 def init_selection_tag_count(comp: component.Slider):
